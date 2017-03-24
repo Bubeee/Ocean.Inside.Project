3e939d3c94465d42d5cf788effef7a31aff96359
Uladzimir Vysotski <Uladzimir_Vysotski@epam.com>
2017-03-24 20:14:05 +0300

    application of free mockup

--- Ocean.Inside.Project/App_Start/BundleConfig.cs
+++ Ocean.Inside.Project/App_Start/BundleConfig.cs
@@ -26,6 +26,8 @@ namespace Ocean.Inside.Project
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").IncludeDirectory("~/css", "*.css"));
         }
     }
 }
